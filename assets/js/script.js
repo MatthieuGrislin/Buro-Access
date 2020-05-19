28ad3632a6a7ceb13540e06ca50f7130fa0a970f
TonyRouyer <tony.rouyer@hotmail.fr>
2020-05-19 10:46:38 +0200

    ajout fonction total

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//affiche la categorie en fonction de celle selectionner
 function HideAndShow(selectedIndex) {
     var menu = document.getElementsByTagName('Section');
     for(menuIndex = 0; menuIndex < menu.length; menuIndex++) {
@@ -10,23 +11,57 @@ function HideAndShow(selectedIndex) {
   }
 //onclick affiche l-item dans le panier, recupere la valeur quantitée et la rentre dans la quantiter du panier
   function addBasket(monId, quantityValue, quantityTarget, resultTarget, unitPrice) {
+    if (document.getElementById(monId).style.display == 'block' ) {
+      alert("L'article est déja dans la panier")
+    }
     document.getElementById(monId).style.display = 'block';
     var getQuantity = document.getElementById(quantityValue).value; //quantityValue = quantitée sur l'index
     document.getElementById(quantityTarget).value = getQuantity; //quantityTarget = champ quantiée dans la modal
     document.getElementById(resultTarget).innerHTML = document.getElementById(quantityTarget).value * unitPrice; //resultTarget = span qui contient le prix dans la modal, unitPrice = prix unitaire de l'item
+    var amount1 =   Number(document.getElementById('claireFontainePrice').innerText),
+        amount2 =   Number(document.getElementById('rametteOfficeDepotPrice').innerText),
+        amount3 =   Number(document.getElementById('cartonOfficeDepotPrice').innerText),
+        amount4 =   Number(document.getElementById('ReyProDesignPrice').innerText),
+        amount5 =   Number(document.getElementById('classeursPrice').innerText),
+        amount6 =   Number(document.getElementById('archiveBoxPrice').innerText),
+        amount7 =   Number(document.getElementById('folderPrice').innerText),
+        amount8 =   Number(document.getElementById('sleevePrice').innerText),
+        amount9 =   Number(document.getElementById('4ColorFluoPrice').innerText),
+        amount10 =  Number(document.getElementById('4ColorPrice').innerText),
+        amount11 =  Number(document.getElementById('styloPaperMatePrice').innerText),
+        amount12 =  Number(document.getElementById('styloBicPrice').innerText),
+        amount13 =  Number(document.getElementById('scotchPrice').innerText),
+        amount14 =  Number(document.getElementById('staplerPrice').innerText),
+        amount15 =  Number(document.getElementById('cutterPrice').innerText),
+        amount16 =  Number(document.getElementById('paperClipsPrice').innerText);
+    var balance = amount1 + amount2 + amount3 + amount4 + amount5 + amount6 + amount7 + amount8 + amount9 + amount10 + amount11 + amount12 + amount13 + amount14 + amount15 + amount16;
+    document.getElementById('amountTotal').innerHTML = balance;
   }
   //onkeyUp recupere la quantitée et la multiplie par le prix unitaire avant de l'écrire dans la span priceID
-  function totalPrice(myId, priceID, oneItemPrice, amountTotal) {
+  function totalPrice(myId, priceID, oneItemPrice) {
     var quantity = document.getElementById(myId).value ; // myId = input quantity de la modal
     var unitPrice =  oneItemPrice; //oneItemPrice = prix unitaire d'un item
     var result = quantity * unitPrice;
     document.getElementById(priceID).innerHTML = result;  //price ID = span prix dans la modal
-    //test pour additioner tout les prix
-    // var balance = document.getElementsByClassName('amount');
-    // for (item = 0; item < balance.length; item++ ) {
-    //   var balance = balance[item].value + balance;
-    // }
-    // document.getElementById(amountTotal).innerHTML = balance;
+    //additioner tout les prix pour l'ecrire dans le prix total
+    var amount1 =   Number(document.getElementById('claireFontainePrice').innerText),
+        amount2 =   Number(document.getElementById('rametteOfficeDepotPrice').innerText),
+        amount3 =   Number(document.getElementById('cartonOfficeDepotPrice').innerText),
+        amount4 =   Number(document.getElementById('ReyProDesignPrice').innerText),
+        amount5 =   Number(document.getElementById('classeursPrice').innerText),
+        amount6 =   Number(document.getElementById('archiveBoxPrice').innerText),
+        amount7 =   Number(document.getElementById('folderPrice').innerText),
+        amount8 =   Number(document.getElementById('sleevePrice').innerText),
+        amount9 =   Number(document.getElementById('4ColorFluoPrice').innerText),
+        amount10 =  Number(document.getElementById('4ColorPrice').innerText),
+        amount11 =  Number(document.getElementById('styloPaperMatePrice').innerText),
+        amount12 =  Number(document.getElementById('styloBicPrice').innerText),
+        amount13 =  Number(document.getElementById('scotchPrice').innerText),
+        amount14 =  Number(document.getElementById('staplerPrice').innerText),
+        amount15 =  Number(document.getElementById('cutterPrice').innerText),
+        amount16 =  Number(document.getElementById('paperClipsPrice').innerText);
+    var balance = amount1 + amount2 + amount3 + amount4 + amount5 + amount6 + amount7 + amount8 + amount9 + amount10 + amount11 + amount12 + amount13 + amount14 + amount15 + amount16;
+    document.getElementById('amountTotal').innerHTML = balance;
   }  
   // onclick supprime l'item selectionner
   function deleteItem(myId) {
