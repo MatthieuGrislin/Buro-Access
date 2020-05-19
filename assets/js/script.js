f5ac83da76a049e2ac2c0e26db360884f3c01aa0
TonyRouyer <tony.rouyer@hotmail.fr>
2020-05-19 11:40:21 +0200

    ajout fonction supprimer pannier et css

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,7 @@ function HideAndShow(selectedIndex) {
     var getQuantity = document.getElementById(quantityValue).value; //quantityValue = quantitée sur l'index
     document.getElementById(quantityTarget).value = getQuantity; //quantityTarget = champ quantiée dans la modal
     document.getElementById(resultTarget).innerHTML = document.getElementById(quantityTarget).value * unitPrice; //resultTarget = span qui contient le prix dans la modal, unitPrice = prix unitaire de l'item
+    //affiche le total des article selectioner dans le panier
     var amount1 =   Number(document.getElementById('claireFontainePrice').innerText),
         amount2 =   Number(document.getElementById('rametteOfficeDepotPrice').innerText),
         amount3 =   Number(document.getElementById('cartonOfficeDepotPrice').innerText),
@@ -35,7 +36,7 @@ function HideAndShow(selectedIndex) {
         amount15 =  Number(document.getElementById('cutterPrice').innerText),
         amount16 =  Number(document.getElementById('paperClipsPrice').innerText);
     var balance = amount1 + amount2 + amount3 + amount4 + amount5 + amount6 + amount7 + amount8 + amount9 + amount10 + amount11 + amount12 + amount13 + amount14 + amount15 + amount16;
-    document.getElementById('amountTotal').innerHTML = balance;
+    document.getElementById('amountTotal').innerHTML = balance.toFixed(2);
   }
   //onkeyUp recupere la quantitée et la multiplie par le prix unitaire avant de l'écrire dans la span priceID
   function totalPrice(myId, priceID, oneItemPrice) {
@@ -61,11 +62,32 @@ function HideAndShow(selectedIndex) {
         amount15 =  Number(document.getElementById('cutterPrice').innerText),
         amount16 =  Number(document.getElementById('paperClipsPrice').innerText);
     var balance = amount1 + amount2 + amount3 + amount4 + amount5 + amount6 + amount7 + amount8 + amount9 + amount10 + amount11 + amount12 + amount13 + amount14 + amount15 + amount16;
-    document.getElementById('amountTotal').innerHTML = balance;
+    document.getElementById('amountTotal').innerHTML = balance.toFixed(2);
   }  
   // onclick supprime l'item selectionner
-  function deleteItem(myId) {
+  function deleteItem(myId, amountId) {
     document.getElementById(myId).style.display = 'none';  //myId =  l'id de l'item
+     //soustrait le montant de l'aticle supprimer et affiche le total
+    var amount1 =   Number(document.getElementById('claireFontainePrice').innerText),
+        amount2 =   Number(document.getElementById('rametteOfficeDepotPrice').innerText),
+        amount3 =   Number(document.getElementById('cartonOfficeDepotPrice').innerText),
+        amount4 =   Number(document.getElementById('ReyProDesignPrice').innerText),
+        amount5 =   Number(document.getElementById('classeursPrice').innerText),
+        amount6 =   Number(document.getElementById('archiveBoxPrice').innerText),
+        amount7 =   Number(document.getElementById('folderPrice').innerText),
+        amount8 =   Number(document.getElementById('sleevePrice').innerText),
+        amount9 =   Number(document.getElementById('4ColorFluoPrice').innerText),
+        amount10 =  Number(document.getElementById('4ColorPrice').innerText),
+        amount11 =  Number(document.getElementById('styloPaperMatePrice').innerText),
+        amount12 =  Number(document.getElementById('styloBicPrice').innerText),
+        amount13 =  Number(document.getElementById('scotchPrice').innerText),
+        amount14 =  Number(document.getElementById('staplerPrice').innerText),
+        amount15 =  Number(document.getElementById('cutterPrice').innerText),
+        amount16 =  Number(document.getElementById('paperClipsPrice').innerText);
+    var balance = amount1 + amount2 + amount3 + amount4 + amount5 + amount6 + amount7 + amount8 + amount9 + amount10 + amount11 + amount12 + amount13 + amount14 + amount15 + amount16;
+    var numberSuppr = document.getElementById(amountId).innerText;
+    balance = balance - numberSuppr ;
+    document.getElementById('amountTotal').innerHTML = balance.toFixed(2);
   }
   // onclick vide le panier
   function deleteAll() {
@@ -73,6 +95,8 @@ function HideAndShow(selectedIndex) {
     for (item = 0; item < itemList.length; item++ ) {
       itemList[item].style.display = 'none';
     }
+    balance = 0;
+    document.getElementById('amountTotal').innerHTML = balance;
   }
   function changeBorder(element) {
     element.style.border = 'solid green 3px';
