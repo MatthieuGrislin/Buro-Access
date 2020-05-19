05ad7a7c2e9b8d23b7fbfe7415c870165b7c96e0
TonyRouyer <tony.rouyer@hotmail.fr>
2020-05-19 11:53:28 +0200

    modif pannier , ajout 2 decimal prix panier

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ function HideAndShow(selectedIndex) {
     document.getElementById(monId).style.display = 'block';
     var getQuantity = document.getElementById(quantityValue).value; //quantityValue = quantitée sur l'index
     document.getElementById(quantityTarget).value = getQuantity; //quantityTarget = champ quantiée dans la modal
-    document.getElementById(resultTarget).innerHTML = document.getElementById(quantityTarget).value * unitPrice; //resultTarget = span qui contient le prix dans la modal, unitPrice = prix unitaire de l'item
+    document.getElementById(resultTarget).innerHTML = (document.getElementById(quantityTarget).value * unitPrice).toFixed(2); //resultTarget = span qui contient le prix dans la modal, unitPrice = prix unitaire de l'item
     //affiche le total des article selectioner dans le panier
     var amount1 =   Number(document.getElementById('claireFontainePrice').innerText),
         amount2 =   Number(document.getElementById('rametteOfficeDepotPrice').innerText),
@@ -43,7 +43,7 @@ function HideAndShow(selectedIndex) {
     var quantity = document.getElementById(myId).value ; // myId = input quantity de la modal
     var unitPrice =  oneItemPrice; //oneItemPrice = prix unitaire d'un item
     var result = quantity * unitPrice;
-    document.getElementById(priceID).innerHTML = result;  //price ID = span prix dans la modal
+    document.getElementById(priceID).innerHTML = result.toFixed(2);  //price ID = span prix dans la modal
     //additioner tout les prix pour l'ecrire dans le prix total
     var amount1 =   Number(document.getElementById('claireFontainePrice').innerText),
         amount2 =   Number(document.getElementById('rametteOfficeDepotPrice').innerText),
